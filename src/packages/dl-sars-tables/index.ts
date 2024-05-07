ebf67d2d39c6eb07feacd5f4c1eff6d0c26f200a
Lyall <lyall.vanderlinde@iriesoft.net>
2024-05-07 09:58:37 +0200

    added deno vscode extension. cache url imports

--- src/packages/dl-sars-tables/index.ts
+++ src/packages/dl-sars-tables/index.ts
@@ -3,34 +3,47 @@ import ky from 'ky';
 import {
     DOMParser,
     Element,
-} from "../../../vendor/deno.land/x/deno_dom@v0.1.45/deno-dom-wasm.ts";
+} from "https://deno.land/x/deno_dom@v0.1.45/deno-dom-wasm.ts";
+//import { stringsFromTable } from "https://deno.land/x/html_table_parser@0.0.2/mod.ts";
+//import { parseDocument } from "https://deno.land/x/html_parser@v0.1.3/src/mod.ts";
+import { existsSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
 
-import { stringsFromTable } from "../../../vendor/deno.land/x/html_table_parser@0.0.2/mod.ts";
-import { parseDocument } from "../../../vendor/deno.land/x/html_parser@v0.1.3/src/mod.ts";
+
 
 const jsonUrl = "https://jsonplaceholder.typicode.com/todos/1";
-const htmlUrl = "https://www.sars.gov.za/tax-rates/income-tax/rates-of-tax-for-individuals/";
+const url = "https://www.sars.gov.za/tax-rates/income-tax/rates-of-tax-for-individuals/";
 
 const baseUrl = "https://www.sars.gov.za";
 const resource = "tax-rates/income-tax/rates-of-tax-for-individuals/";
 
+// get the base64 hash of the url to download
+const hash = btoa(url);
 
+const cachedUrl = `${hash}.html`;
+let html = "";
 
-const html = await ky.get(htmlUrl).text();
-//const domParser = new DOMParser().parseFromString("response")!;
-
-// TODO: get first html table
-//const table = doc.querySelector(".ms-rteTable-default")!;
+// check if the cached file exists in the folder
+if (existsSync(cachedUrl)) {
+    html = Deno.readTextFileSync(cachedUrl);
+    // TODO: parse the cached file
+} else {
+    // download the file and parse it
+    html = await ky.get(url).text();
+    // save the file in the cache
+    Deno.writeTextFileSync(cachedUrl, html);
+}
 
 const doc = new DOMParser().parseFromString(html, "text/html");
 if (doc === null) {
     throw new Error("Failed to parse HTML");
 }
 
-const document = parseDocument(html);
-
-const p = doc.querySelector(".ms-rteTable-default");
-const table: string[][] = stringsFromTable(document);
+const table = doc.querySelector(".ms-rteTable-default");
 
 
-console.log(p?.innerHTML);
+// table.
+
+// const table: string[][] = stringsFromTable(table);
+
+
+// console.log(p?.innerHTML);
